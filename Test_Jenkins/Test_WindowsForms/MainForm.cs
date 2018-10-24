a0ee1f390a3d168481e1168f024659e465ebaeb6
ibrahimahmed-aurea <ibrahim.ahmed@crossover.com>
2018-10-24 16:47:15 +0200

    Creating the develop branch

--- Test_Jenkins/Test_WindowsForms/MainForm.cs
+++ Test_Jenkins/Test_WindowsForms/MainForm.cs
@@ -9,11 +9,16 @@ using System.Windows.Forms;
 
 namespace Test_WindowsForms
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }

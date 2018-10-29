a870358c709ef3d8705907e948b26e356a97f227
ibrahimahmed-aurea <ibrahim.ahmed@crossover.com>
2018-10-29 21:22:16 +0200

    Adding the test project

---
+++ Test_Jenkins/UnitTestProject/MathOperationTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Test_WindowsForms;
+
+namespace UnitTestProject
+{
+    [NUnit.Framework.TestFixture]
+    public class MathOperationTests
+    {
+        [Test]
+        public void Add_Positive_ReturnSum()
+        {
+            int reuslt = MathOperations.Add(5, 6);
+            Assert.AreEqual(reuslt, 11);
+        }
+
+        [Test]
+        public void Add_Negative_ReturnSum()
+        {
+            int reuslt = MathOperations.Add(-5, -6);
+            Assert.AreEqual(reuslt, -11);
+        }
+    }
+}
---
+++ Test_Jenkins/UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f384b697-10e2-4c50-914a-d12450a4e8d9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

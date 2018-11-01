b896f3da0bb931ebc7dcb1df12574f0f6d218117
ibrahimahmed-aurea <ibrahim.ahmed@crossover.com>
2018-11-01 12:55:58 +0200

    Fix the build

--- dotnet/source/Wms/Mobile/Server/Properties/AssemblyInfo.cs
+++ dotnet/source/Wms/Mobile/Server/Properties/AssemblyInfo.cs
@@ -2,6 +2,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Imi.Framework.Shared;
+using Imi.Framework.Versioning;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
--- dotnet/source/Wms/Mobile/Server/UI/Properties/AssemblyInfo.cs
+++ dotnet/source/Wms/Mobile/Server/UI/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using Imi.Framework.Shared;
+using Imi.Framework.Versioning;
 //
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information

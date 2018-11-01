70bb6b55f719d1570e529df7d087e146d829bf19
ibrahimahmed-aurea <ibrahim.ahmed@crossover.com>
2018-11-02 00:41:40 +0200

    fix jenkins

--- dotnet/test.bat
+++ dotnet/test.bat
@@ -2,7 +2,7 @@ set MsBuild=C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
 set TrackDrive=N:
 
 Echo Build the unit tests solution
-%MsBuild% /p:Configuration=Release /p:Platform="Any CPU" /v:m "%TrackDrive%\source\UnitTests\WMSUnitTests.sln"
+%MsBuild% /p:Configuration=Release /p:Platform="Any CPU" /tv:14.0  /v:m "%TrackDrive%\source\UnitTests\WMSUnitTests.sln"
 
 Echo Start testing..
 "C:\Program Files (x86)\NUnit.org\nunit-console\nunit3-console.exe" "%TrackDrive%\source\UnitTests\WMSUnitTests\bin\Release\WMSUnitTests.dll"

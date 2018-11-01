04837807d3110ac66a5390d256010fd13dee0a68
ibrahimahmed-aurea <ibrahim.ahmed@crossover.com>
2018-11-02 00:24:37 +0200

    fix jenkins

--- buil.bat
+++ buil.bat
@@ -1,3 +1,3 @@
 set MsBuild=C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
 
-%MsBuild%  /p:Configuration=Release /p:Platform="Any CPU" /v:m Test_Jenkins/Test_Jenkins.sln
+%MsBuild%  /p:Configuration=Release /p:Platform="Any CPU" /tv:14.0 /v:m  Test_Jenkins/Test_Jenkins.sln
